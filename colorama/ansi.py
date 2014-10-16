c1a32e1d437bd7dd17d0e76479d85bbb8a561db2
omoman <mannysanchez998@gmail.com>
2014-10-15 21:48:53 -0700

    Added colorama module for printing in color

---
+++ colorama/ansi.py
@@ -0,0 +1,95 @@
+# Copyright Jonathan Hartley 2013. BSD 3-Clause license, see LICENSE file.
+'''
+This module generates ANSI character codes to printing colors to terminals.
+See: http://en.wikipedia.org/wiki/ANSI_escape_code
+'''
+
+CSI = '\033['
+OSC = '\033]'
+BEL = '\007'
+
+
+def code_to_chars(code):
+    return CSI + str(code) + 'm'
+
+
+class AnsiCodes(object):
+    def __init__(self, codes):
+        for name in dir(codes):
+            if not name.startswith('_'):
+                value = getattr(codes, name)
+                setattr(self, name, code_to_chars(value))
+
+
+class AnsiCursor(object):
+    def UP(self, n=1):
+        return CSI + str(n) + "A"
+    def DOWN(self, n=1):
+        return CSI + str(n) + "B"
+    def FORWARD(self, n=1):
+        return CSI + str(n) + "C"
+    def BACK(self, n=1):
+        return CSI + str(n) + "D"
+    def POS(self, x=1, y=1):
+        return CSI + str(y) + ";" + str(x) + "H"
+
+def set_title(title):
+    return OSC + "2;" + title + BEL
+
+def clear_screen(mode=2):
+    return CSI + str(mode) + "J"
+
+def clear_line(mode=2):
+    return CSI + str(mode) + "K"
+
+
+class AnsiFore:
+    BLACK        = 30
+    RED          = 31
+    GREEN        = 32
+    YELLOW       = 33
+    BLUE         = 34
+    MAGENTA      = 35
+    CYAN         = 36
+    WHITE        = 37
+    RESET        = 39
+    LIGHTBLACK   = 90
+    LIGHTRED     = 91
+    LIGHTGREEN   = 92
+    LIGHTYELLOW  = 93
+    LIGHTBLUE    = 94
+    LIGHTMAGENTA = 95
+    LIGHTCYAN    = 96
+    LIGHTWHITE   = 97
+
+
+class AnsiBack:
+    BLACK        = 40
+    RED          = 41
+    GREEN        = 42
+    YELLOW       = 43
+    BLUE         = 44
+    MAGENTA      = 45
+    CYAN         = 46
+    WHITE        = 47
+    RESET        = 49
+    LIGHTBLACK   = 100
+    LIGHTRED     = 101
+    LIGHTGREEN   = 102
+    LIGHTYELLOW  = 103
+    LIGHTBLUE    = 104
+    LIGHTMAGENTA = 105
+    LIGHTCYAN    = 106
+    LIGHTWHITE   = 107
+
+
+class AnsiStyle:
+    BRIGHT    = 1
+    DIM       = 2
+    NORMAL    = 22
+    RESET_ALL = 0
+
+Fore = AnsiCodes( AnsiFore )
+Back = AnsiCodes( AnsiBack )
+Style = AnsiCodes( AnsiStyle )
+Cursor = AnsiCursor()
