b4a240683b536cbf326cf5f1a6275450617eb7d5
omoman <mannysanchez998@gmail.com>
2014-10-15 22:10:14 -0700

    added colored info print functions and added error message for invalid swipe

--- app.py
+++ app.py
@@ -23,8 +23,32 @@ def parse_number (carddump):
     else:
         return None
 
+'''
+Print strings in different colors
+@param string {String}
+@return none
+'''
+def print_error (string):
+    print '{}{}{}'.format(Fore.RED, Style.BRIGHT, string)
+    print Style.RESET_ALL
+
+def print_success (string):
+    print '{}{}{}'.format(Fore.GREEN, Style.BRIGHT, string)
+    print Style.RESET_ALL
+
+def print_status (string):
+    print '{}{}{}'.format(Fore.CYAN, Style.BRIGHT, string)
+    print Style.RESET_ALL
+
 if __name__ == '__main__':
     # Main event loop
     while 1:
         card_dump = getswipe("Swipe your R'card ID\n")
-        print parse_number(card_dump)
+        card_number = parse_number(card_dump)
+        
+        # If we were NOT able to get a valid number
+        # from the swipe, let the user know and skip everything
+        # else
+        if card_number == None:
+           print_error ("Invalid swipe. Try again") 
+           continue
